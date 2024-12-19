5247f4142bf176dde069983fc4504d47d8c6211f
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-12-19 08:59:51 -0300

    test: testando a criacao da classe Node

--- __tests__/06-listas-ligadas/Node.test.ts
+++ __tests__/06-listas-ligadas/Node.test.ts
@@ -1,3 +1,8 @@
+import Node from "../../src/06-listas-ligadas/Node";
+
 describe("LinkedList: -> Node: Classe auxiliar para representar a cabeça (head) e outros elementos da lista ligada", () => {
-   it.todo("");
+   it("Deve criar o objeto Node corretamente", () => {
+      const node = new Node(12);
+      expect(node).toEqual({ element: 12, next: undefined });
+   });
 });
