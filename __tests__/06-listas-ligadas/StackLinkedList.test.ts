c5368297ca4b92af0739c5303f36b5191014903b
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-12-26 09:31:54 -0300

    test: retornando o tamanho da pilha e inserindo elementos na pilha

--- __tests__/06-listas-ligadas/StackLinkedList.test.ts
+++ __tests__/06-listas-ligadas/StackLinkedList.test.ts
@@ -5,4 +5,20 @@ describe("StackLinkedList", () => {
       const stackLinkedList = new StackLinkedList();
       expect(stackLinkedList).toBeInstanceOf(StackLinkedList);
    });
+
+   it("Deve retornar o tamanho da pilha", () => {
+      const stackLinkedList = new StackLinkedList();
+      expect(stackLinkedList.size()).toBe(0);
+   });
+
+   it("Deve inserir dados da pilha", () => {
+      const stackLinkedList = new StackLinkedList();
+
+      stackLinkedList.push(10);
+      stackLinkedList.push(20);
+
+      expect(stackLinkedList.size()).toBe(2);
+   });
+
+   it("Deve remover dados da pilha", () => {});
 });
--- src/06-listas-ligadas/StackLinkedList.ts
+++ src/06-listas-ligadas/StackLinkedList.ts
@@ -1,3 +1,19 @@
-class StackLinkedList {}
+import DoublyLinkedList from "./DoublyLinkedList";
+
+class StackLinkedList<T> {
+   private items: DoublyLinkedList<T>;
+
+   constructor() {
+      this.items = new DoublyLinkedList();
+   }
+
+   size() {
+      return this.items.size();
+   }
+
+   push(element: T) {
+      this.items.push(element);
+   }
+}
 
 export default StackLinkedList;
