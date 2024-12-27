99788ff87b936df6300db2e0ad5d1aa69487bb0b
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-12-27 08:48:48 -0300

    test: declarando testes que serao feitos

--- __tests__/07-conjuntos/Set.test.ts
+++ __tests__/07-conjuntos/Set.test.ts
@@ -5,4 +5,18 @@ describe("Set: Conjuntos", () => {
       const set = new Set();
       expect(set).toBeInstanceOf(Set);
    });
+
+   it.todo("add(element): adiciona um novo element ao conjunto");
+
+   it.todo("delete(element): remove element do conjunto");
+
+   it.todo("has(element): devolve true se element estiver no conjunto, e false caso contrário.");
+
+   it.todo("clear(): remove todos os elementos do conjunto");
+
+   it.todo(
+      "size(): devolve quantos elementos estão contidos no conjunto. É semelhante á propriedade length de um array."
+   );
+
+   it.todo("values(): devolve um array com todos os valores (elementos) que estão no conjunto.");
 });
