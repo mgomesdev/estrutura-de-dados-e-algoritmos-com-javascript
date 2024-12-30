642f97eed83f15d46da5636a641339f82adc7a86
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-12-30 11:31:23 -0300

    test: criando suite de teste para SetNative

---
+++ __tests__/07-conjuntos/SetNative.test.ts
@@ -0,0 +1,15 @@
+describe("ES6 - A classe Set", () => {
+   it.todo("Implementando a classe Set");
+
+   it.todo("Operações com a classe set");
+
+   it.todo("Simulando a operação de união");
+
+   it.todo("Simulando a operação de intersecção");
+
+   it.todo("Simulando a operação de diferença");
+
+   it.todo("Usando o operador de espalhamento");
+
+   it.todo("Multiconjuntos ou bags");
+});
