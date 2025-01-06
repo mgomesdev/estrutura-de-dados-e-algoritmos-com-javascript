1cd51098d5ebf7dc197723cc20dcf5e43eff25a8
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-01-06 08:47:34 -0300

    test: put(key, value): adiciona um novo item.

--- __tests__/08-dicionarios-e-hashes/HashTableSeparateChaining.test.ts
+++ __tests__/08-dicionarios-e-hashes/HashTableSeparateChaining.test.ts
@@ -1,3 +1,4 @@
+import LinkedList from "../../src/06-listas-ligadas/LinkedList";
 import HashTableSeparateChaining from "../../src/08-dicionarios-e-hashes/HashTableSeparateChaining";
 
 describe("HashTableSeparateChaining", () => {
@@ -6,7 +7,13 @@ describe("HashTableSeparateChaining", () => {
       expect(hashTableSeparateChaining).toBeInstanceOf(HashTableSeparateChaining);
    });
 
-   it.todo("put(key, value): adiciona um novo item.");
+   it("put(key, value): adiciona um novo item.", () => {
+      const hashTableSeparateChaining = new HashTableSeparateChaining();
+
+      const add = hashTableSeparateChaining.put("nome1", "matheus");
+
+      expect(add).toBeTruthy();
+   });
 
    it.todo("get(key): obtem um valor dava uma chave.");
 
--- src/08-dicionarios-e-hashes/HashTableSeparateChaining.ts
+++ src/08-dicionarios-e-hashes/HashTableSeparateChaining.ts
@@ -1,8 +1,41 @@
+import LinkedList from "../06-listas-ligadas/LinkedList";
+import ValuePair from "./ValuePair";
+
 class HashTableSeparateChaining<T> {
    private table: { [key: string]: T };
 
    constructor() {
       this.table = {};
+   }
+
+   loseloseHashCode(key: T) {
+      if (typeof key === "number") return key;
+
+      const tableKey = key as string;
+      let hash = 0;
+
+      for (let i = 0; i < tableKey.length; i++) {
+         hash += tableKey.charCodeAt(i);
+      }
+
+      return hash % 37;
+   }
+
+   hashCode(key: T) {
+      return this.loseloseHashCode(key);
+   }
+
+   put(key: T, value: T) {
+      if (key !== null && value !== null) {
+         const position = this.hashCode(key as T);
+
+         if (this.table[position] === undefined) (this.table[position] as object) = new LinkedList();
+
+         (this.table[position] as any).push(new ValuePair(key as string, value));
+         return true;
+      }
+
+      return false;
    }
 }
 
